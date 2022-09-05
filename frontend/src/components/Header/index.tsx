7022a9fb86f6903ca399467443c5914b58933b65
Natzujj <gabrielnatsujake@gmail.com>
2022-09-05 10:56:55 -0300

    Other Components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+import './style.css'
+
+function Header() {
+    return (
+        <>
+            <header>
+                <div className="dsmeta-logo-container">
+                    <img src={logo} alt="logo DSMeta"/>
+                        <h1>DsMeta</h1>
+                        <p>Desenvolvido por
+                            <a href="https://www.instagram.com/devsuperior.ig" target="_blank"> @devsuperior.ig</a>
+                        </p>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
